3a86c678c24a5ff96fba042dbf627ab828fd5943
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2025-06-09 00:39:43 +0900

    Refactor node structure and expression parsing for improved clarity
    - Replaced the NodeKind enum with a more streamlined Node enum that directly represents arithmetic operations.
    - Updated the expr and mul functions to utilize pattern matching for operator handling, enhancing readability.
    - Simplified the primary function to directly return Node instances for numbers.
    - Adjusted the generate function to work with the new Node structure, maintaining functionality for generating ARM64 assembly code.

--- rustc/src/main.rs
+++ rustc/src/main.rs
@@ -15,27 +15,7 @@ fn main() {
     println!(".section __TEXT,__text");
     println!(".globl _main");
     println!("_main:");
-
     generate(&node);
-    // println!("node: {:?}", node);
-    // First number
-    // let first_tok = iter.next().unwrap();
-    // let num = expect_number(&first_tok, &exp);
-    // println!("    mov x0, #{}", num);
-    // // Remaining operator-number pairs
-    // while let Some(tok) = iter.next() {
-    //     if at_eof(&tok) {
-    //         break;
-    //     }
-    //     let op = expect_operator(&tok, &exp);
-    //     let num_tok = iter.next().unwrap();
-    //     let num = expect_number(&num_tok, &exp);
-    //     if op == '+' {
-    //         println!("    add x0, x0, #{}", num);
-    //     } else {
-    //         println!("    sub x0, x0, #{}", num);
-    //     }
-    // }
     // Pop final result and restore stack pointer
     println!("    ldr x0, [sp]");
     println!("    add sp, sp, #16");
--- rustc/src/node.rs
+++ rustc/src/node.rs
@@ -2,40 +2,27 @@ use crate::{TokenIter, TokenKind};
 use std::iter::Peekable;
 
 #[derive(Debug)]
-pub enum NodeKind {
+pub enum Node {
     Num(u64),
-    Add,
-    Sub,
-    Mul,
-    Div,
-}
-
-#[derive(Debug)]
-pub struct Node {
-    pub kind: NodeKind,
-    pub lhs: Option<Box<Node>>,
-    pub rhs: Option<Box<Node>>,
-}
-
-fn new_node(kind: NodeKind, lhs: Node, rhs: Node) -> Node {
-    Node {
-        kind,
-        lhs: Some(Box::new(lhs)),
-        rhs: Some(Box::new(rhs)),
-    }
+    Add(Box<Node>, Box<Node>),
+    Sub(Box<Node>, Box<Node>),
+    Mul(Box<Node>, Box<Node>),
+    Div(Box<Node>, Box<Node>),
 }
 
 pub fn expr(toks: &mut Peekable<TokenIter>) -> Node {
     let mut lhs = mul(toks);
     while let Some(tok) = toks.peek() {
-        if tok.kind == TokenKind::Operator('+') {
-            toks.next();
-            lhs = new_node(NodeKind::Add, lhs, mul(toks));
-        } else if tok.kind == TokenKind::Operator('-') {
-            toks.next();
-            lhs = new_node(NodeKind::Sub, lhs, mul(toks));
-        } else {
-            break;
+        match tok.kind {
+            TokenKind::Operator('+') => {
+                toks.next();
+                lhs = Node::Add(Box::new(lhs), Box::new(mul(toks)));
+            }
+            TokenKind::Operator('-') => {
+                toks.next();
+                lhs = Node::Sub(Box::new(lhs), Box::new(mul(toks)));
+            }
+            _ => break,
         }
     }
     lhs
@@ -44,14 +31,16 @@ pub fn expr(toks: &mut Peekable<TokenIter>) -> Node {
 fn mul(toks: &mut Peekable<TokenIter>) -> Node {
     let mut lhs = primary(toks);
     while let Some(tok) = toks.peek() {
-        if tok.kind == TokenKind::Operator('*') {
-            toks.next();
-            lhs = new_node(NodeKind::Mul, lhs, primary(toks));
-        } else if tok.kind == TokenKind::Operator('/') {
-            toks.next();
-            lhs = new_node(NodeKind::Div, lhs, primary(toks));
-        } else {
-            break;
+        match tok.kind {
+            TokenKind::Operator('*') => {
+                toks.next();
+                lhs = Node::Mul(Box::new(lhs), Box::new(primary(toks)));
+            }
+            TokenKind::Operator('/') => {
+                toks.next();
+                lhs = Node::Div(Box::new(lhs), Box::new(primary(toks)));
+            }
+            _ => break,
         }
     }
     lhs
@@ -59,41 +48,34 @@ fn mul(toks: &mut Peekable<TokenIter>) -> Node {
 
 fn primary(toks: &mut Peekable<TokenIter>) -> Node {
     let tok = toks.next().unwrap();
-    match tok.kind {
-        TokenKind::Number(n) => Node {
-            kind: NodeKind::Num(n),
-            lhs: None,
-            rhs: None,
-        },
-        _ => unreachable!(),
+    if let TokenKind::Number(n) = tok.kind {
+        Node::Num(n)
+    } else {
+        unreachable!()
     }
 }
 
 pub fn generate(node: &Node) {
-    match &node.kind {
-        NodeKind::Num(n) => {
-            // Load immediate and push to stack
+    match node {
+        Node::Num(n) => {
             println!("    mov x0, #{}", n);
             println!("    sub sp, sp, #16");
             println!("    str x0, [sp]");
         }
-        NodeKind::Add => {
-            generate(node.lhs.as_ref().unwrap());
-            generate(node.rhs.as_ref().unwrap());
-            // Pop right into x1
+        Node::Add(lhs, rhs) => {
+            generate(lhs);
+            generate(rhs);
             println!("    ldr x1, [sp]");
             println!("    add sp, sp, #16");
-            // Pop left into x0
             println!("    ldr x0, [sp]");
             println!("    add sp, sp, #16");
             println!("    add x0, x0, x1");
-            // Push result
             println!("    sub sp, sp, #16");
             println!("    str x0, [sp]");
         }
-        NodeKind::Sub => {
-            generate(node.lhs.as_ref().unwrap());
-            generate(node.rhs.as_ref().unwrap());
+        Node::Sub(lhs, rhs) => {
+            generate(lhs);
+            generate(rhs);
             println!("    ldr x1, [sp]");
             println!("    add sp, sp, #16");
             println!("    ldr x0, [sp]");
@@ -102,9 +84,9 @@ pub fn generate(node: &Node) {
             println!("    sub sp, sp, #16");
             println!("    str x0, [sp]");
         }
-        NodeKind::Mul => {
-            generate(node.lhs.as_ref().unwrap());
-            generate(node.rhs.as_ref().unwrap());
+        Node::Mul(lhs, rhs) => {
+            generate(lhs);
+            generate(rhs);
             println!("    ldr x1, [sp]");
             println!("    add sp, sp, #16");
             println!("    ldr x0, [sp]");
@@ -113,9 +95,9 @@ pub fn generate(node: &Node) {
             println!("    sub sp, sp, #16");
             println!("    str x0, [sp]");
         }
-        NodeKind::Div => {
-            generate(node.lhs.as_ref().unwrap());
-            generate(node.rhs.as_ref().unwrap());
+        Node::Div(lhs, rhs) => {
+            generate(lhs);
+            generate(rhs);
             println!("    ldr x1, [sp]");
             println!("    add sp, sp, #16");
             println!("    ldr x0, [sp]");
@@ -125,5 +107,4 @@ pub fn generate(node: &Node) {
             println!("    str x0, [sp]");
         }
     }
-
 }
